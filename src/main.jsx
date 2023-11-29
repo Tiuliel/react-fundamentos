e2a7d5b6e6e22fb1cdf047083b951704a99b763b
Eliel Moura <eliellpf@gmail.com>
2023-11-29 10:29:51 -0300

    reset geral

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,13 @@
+/*  Importação das libs ESSENCIAIS (React e ReactDOM) */
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
 
+/* Importação do primeiro/principal  componete, 
+arquivo em que carregamos os elementos da página (como se fosse um index.html) */
+import App from './App.jsx'
+
+
+/* Inicialização dos recursos do React, manipulando a div#root existente no index.html */
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
