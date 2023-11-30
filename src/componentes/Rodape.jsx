dc60cfa5f32a2d7a4e2421bc0132a747c6a5d65e
Eliel Moura <eliellpf@gmail.com>
2023-11-30 10:00:30 -0300

    css

--- src/componentes/Rodape.jsx
+++ src/componentes/Rodape.jsx
@@ -1,8 +1,14 @@
 function Rodape() {
+  const exemploEstilos = {
+    backgroundColor: "lightblue",
+    padding: "2%",
+    color: "blue",
+    border: "Solid 4px #f00",
+  };
+
   return (
-    <footer>
-      {" "}
-      <h2>Rodapé da aplicaçao</h2>
+    <footer style={exemploEstilos}>
+      <h2 style={{ textAlign: "center" }}>Rodapé da aplicaçao</h2>
     </footer>
   );
 }
