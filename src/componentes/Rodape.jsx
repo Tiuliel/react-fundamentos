1c85a648ec2b3574cd6fcadaa48072f0ff322ff6
Eliel Moura <eliellpf@gmail.com>
2023-12-01 11:13:54 -0300

    correção do exercício

--- src/componentes/Rodape.jsx
+++ src/componentes/Rodape.jsx
@@ -1,12 +1,13 @@
 import styled from "styled-components";
 
 const StyledRodape = styled.footer`
+  background: linear-gradient(to right, #3498db, #2c3e50);
   margin: 0;
+  color: white;
+  height: 30vh;
   display: flex;
   align-items: center;
   justify-content: center;
-  height: 30vh;
-  background: linear-gradient(to right, #3498db, #2c3e50);
 `;
 function Rodape() {
   return (
