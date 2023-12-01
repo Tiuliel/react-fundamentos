69c4c46ecf860018dccfc5635aa2354cdbbcb899
Eliel Moura <eliellpf@gmail.com>
2023-12-01 11:45:34 -0300

    correção do desafio

--- src/componentes/Rodape.jsx
+++ src/componentes/Rodape.jsx
@@ -1,14 +1,5 @@
-import styled from "styled-components";
+import { StyledRodape } from "../styles/styles.js";
 
-const StyledRodape = styled.footer`
-  background: linear-gradient(to right, #3498db, #2c3e50);
-  margin: 0;
-  color: white;
-  height: 30vh;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
 function Rodape() {
   return (
     <StyledRodape>
---
+++ src/styles/styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const StyledRodape = styled.footer`
+  background: linear-gradient(to right, #3498db, #2c3e50);
+  margin: 0;
+  color: white;
+  height: 30vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
