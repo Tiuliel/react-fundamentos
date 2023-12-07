ea3d6d843e04143bdc2941cf78c77ea1875092d0
Eliel Moura <eliellpf@gmail.com>
2023-12-07 08:48:31 -0300

    update conteudo

--- src/componentes/Conteudo.jsx
+++ src/componentes/Conteudo.jsx
@@ -15,6 +15,13 @@ const StyledConteudo = styled.main`
     padding: 0.2rem 0;
   }
 
+  .filtros {
+    margin: 1rem 0;
+    padding: 1rem 0;
+    border-top: solid 2px;
+    border-bottom: solid 2px;
+  }
+
   @media (min-width: 650px) {
     .artigos {
       display: flex;
@@ -29,6 +36,9 @@ const StyledConteudo = styled.main`
 `;
 
 function Conteudo() {
+  const aplicarFiltro = () => {
+    console.log(event.currentTarget.innerText);
+  };
   return (
     <StyledConteudo>
       <h2>Conteúdo da aplicação</h2>
@@ -39,6 +49,15 @@ function Conteudo() {
         maiores cumque quaerat optio consequuntur neque? Asperiores officia
         reprehenderit natus libero non exercitationem vitae.
       </p>
+
+      <div className="filtros">
+        <p>
+          <b>Filtrar por: </b>{" "}
+          <button onClick={aplicarFiltro}>Front-End</button>
+          <button onClick={aplicarFiltro}>Back-End</button>
+          <button onClick={aplicarFiltro}>Design</button>
+        </p>
+      </div>
 
       <div className="artigos">
         {cursos.map((curso) => (
